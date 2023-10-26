46480a038929d72cb1cc8f598aaad6b528a1d7da
Nick Watson <nicowatson1234@gmail.com>
2023-10-26 02:45:26 -0700

    test run

---
+++ Login_History.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Script:                     Login History  
+# Author:                     Nicolaus Watson  
+# Date of latest revision:    10/26/2024  
+# Purpose:                    To check login history 
+
+# Declaration of functions
+
+# Basic function
+login_history () {
+  last | head -3 # last five logins
+  echo This is the login history 
+}
+
+# Main
+login_history
+login_history
+login_history
+
+
+# End
