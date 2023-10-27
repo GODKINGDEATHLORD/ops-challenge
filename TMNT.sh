0902e5752c06772ff5e7a8ff596dc9637dc8e084
Nick Watson <nicowatson1234@gmail.com>
2023-10-26 23:32:49 -0700

    added a breakdown

--- TMNT.sh
+++ TMNT.sh
@@ -5,6 +5,8 @@
 # Date of latest revision:      10/24/2023
 # Purpose:                      To make Teenage Mutant Ninja Turtle portfolios
 
+#                               What we did here is we created for different folders using the "mkdir" command , then we created an array with the folder names we could refer to them later, anfter that we created a file in each of them called "file.txt". This could have been any  type of file though. So we looped through the array by going through the list one by one. THe " for dir in"${directores[@]}" told the computer to go throught the folders one at a time. Touch creates the file
+
 # Create directories
 mkdir Leonardo
 mkdir Raphael
@@ -14,7 +16,6 @@ mkdir Micheangelo
 # Create an array with directory names
 directories=("Leonardo" "Raphael" "Donatello" "Micheangelo")
 
-# Main
 # Loop through the array and create a .txt file in each directory
 for dir in "${directories[@]}"; do
     touch "$dir/file.txt"
