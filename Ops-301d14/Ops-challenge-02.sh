105f9fd94e70f7d3a63a4991edae57a5e7ca899c
Nick Watson <nicowatson1234@gmail.com>
2023-11-29 03:08:39 -0800

    not happy with it, will adjust

--- Ops-301d14/Ops-challenge-02.sh
+++ Ops-301d14/Ops-challenge-02.sh
@@ -22,7 +22,13 @@ echo $day
 
 echo "Current Date: $day-$month-$year"
 echo "Append this to the file test.txt"
-echo "Current Date: $day-$month-$year" >> test,txt
+echo "Current Date: $day-$month-$year" >> test.txt
+
+# Copies /var/log/syslog to the current working directory
+destination_filename="syslog_backup_$(date +%Y%m%d).txt"
+echo "Copying /var/log/syslog to current directory as $destination_filename"
+cp /var/log/syslog "./$destination_filename"
+
 
 # End
 # Notes >= overwrite >>= append or add on to  >= means carrot
