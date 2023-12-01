56a330004c7546bb7b5585fb5f91c8e54fd25dd9
Nick Watson <nicowatson1234@gmail.com>
2023-12-01 05:39:33 -0800

    yes

--- Ops-301d14/Ops-challenge-04.sh
+++ Ops-301d14/Ops-challenge-04.sh
@@ -1,1 +1,41 @@
-yello
+#!/bin/bash
+
+#  print network adapter information
+function print_ip_info() {
+    echo "Network Adapter Information:"
+    ifconfig
+}
+
+#  system loop
+while true; do
+    clear
+    echo "Menu:"
+    echo "1. Hello world"
+    echo "2. Ping self"
+    echo "3. IP info"
+    echo "4. Exit"
+    read -p "Please enter your choice: " choice
+
+    if [[ $choice == 1 ]]; then
+        echo "Hello world!"
+        read -p "Press Enter to continue"
+
+    elif [[ $choice == 2 ]]; then
+        ping -c 4 127.0.0.1
+        read -p "Press Enter to continue"
+
+    elif [[ $choice == 3 ]]; then
+        print_ip_info
+        read -p "Press Enter to continue"
+
+    elif [[ $choice == 4 ]]; then
+        echo "Exiting..."
+        exit 0
+
+    else
+        echo "Invalid choice"
+        read -p "Press Enter to continue"
+    fi
+done
+
+# utilized chatgpt , and the github 
