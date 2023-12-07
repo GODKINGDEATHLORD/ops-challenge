06ce4767eac48733387ac8a82cf577a81a6dd23a
Nick Watson <nicowatson1234@gmail.com>
2023-12-06 23:41:07 -0800

    yes

---
+++ Ops-301d14/Ops-Challenge8.py
@@ -0,0 +1,16 @@
+# Define a list of ten string elements
+string_list = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi", "lemon"]
+
+# Print the fourth element of the list
+print("The fourth element is:", string_list[3])
+
+# Print the sixth through tenth element of the list
+print("The sixth through tenth elements are:", string_list[5:])
+
+# Change the value of the seventh element to "onion"
+string_list[6] = "onion"
+
+# Print the updated list
+print("Updated list with the seventh element changed to 'onion':", string_list)
+
+# Resources ( Chat Gpt)
