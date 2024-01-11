5b09250bdc0e434448999d08661e8582db2952bb
Nick Watson <nicowatson1234@gmail.com>
2024-01-10 19:35:45 -0800

    yes

--- uptime_sensor_tool.py
+++ uptime_sensor_tool.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 
+# References: https://gist.github.com/bockor/50fb07e0edaccbd879f2f4af8c428857 , https://gist.github.com/bockor/50fb07e0edaccbd879f2f4af8c428857, 
+# https://realpython.com/python-f-strings/ , Also worked with Aaron Imbrock
 # imports datetime mode & os(gives you access to a bunch of commands that are specific to the operating system)
 from datetime import datetime 
-import os
+import sys
 import time
 
 from pythonping import ping
-# will reference these two variables on line 27
-ip = 'localhost'
+# will reference these two variables on line 27,    sys.argv will give contents within contents 
+# ip = 'localhost'
 count = 1
+ip = sys.argv[1]
+
 
 # define timestamp and assign timestamp to variable now , from datetime its referencing the file called datetime 
 def timestamp():
@@ -31,29 +35,6 @@ if __name__ == '__main__':
         else:
             status = 'Network Down'
 
-        time.sleep(2) 
         print(f"{timestamp} {status} to {ip}")
-
-# Example output: 2020-10-05 17:57:57.510261 Network Active to 8.8.8.8
-
-
-
-# Requirements
-# In Python, create an uptime sensor tool that uses ICMP packets to evaluate if hosts on the LAN are up or down.
-
-# The script must:
-
-# Transmit a single ICMP (ping) packet to a specific IP every two seconds.
-# Evaluate the response as either success or failure.
-# Assign success or failure to a status variable.
-# For every ICMP transmission attempted, print the status variable along with a comprehensive timestamp and destination IP tested.
-# Example output: 2020-10-05 17:57:57.510261 Network Active to 8.8.8.8
-
-# Stretch Goals (Optional Objectives)
-# In Python, add the below features to your uptime sensor tool.
-
-# The script must:
-
-# Save the output to a text file as a log of events.
-# Accept user input for target IP address.
-
+        time.sleep(2) 
+       
