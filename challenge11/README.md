92351a2a8d5be42f835c9a40ae647b97365ac880
Nick Watson <nicowatson1234@gmail.com>
2024-01-23 16:33:17 -0800

    hellow world works

---
+++ challenge11/README.md
@@ -0,0 +1,10 @@
+# Challenge 11
+
+Summary ...
+
+## Installation
+```linux
+python3 -m venv .venv
+source .venv/bin/acticate
+pip install -r requirements.txt
+```
---
+++ challenge11/ops-401d14-challenge11.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import os       # stdlib's come first (top of the file, listed alphabetically)
+
+import scapy    # third-party (aka pypi installed come second)
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    print("I exist")
+
+
+
+
+
