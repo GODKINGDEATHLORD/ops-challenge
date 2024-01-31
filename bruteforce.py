c0469d40c0d12acc0196b67033a3af3566d8637f
Nick Watson <nicowatson1234@gmail.com>
2024-01-30 23:18:28 -0800

    yes

---
+++ bruteforce.py
@@ -0,0 +1,13 @@
+# Ops Challenge: Automated Brute Force Wordlist Attack Tool Part 2 of 3
+# Overview
+# A brute force attack is a type of computer hack that relies on guessing possible combinations of a targeted password until the correct password is discovered. Today you will continue to develop a custom tool that performs brute force attacks.
+
+# Resources
+# How to Make an SSH Brute-Forcer in Python
+# How to Execute Shell Commands in a Remote Machine using Python - Paramiko
+# Requirements
+# Add to your Python brute force tool the capability to:
+
+# Authenticate to an SSH server by its IP address.
+# Assume the username and IP are known inputs and attempt each word on the provided word list until successful login takes place.
+# Note: Stay out of trouble! Restrict this kind of traffic to your local network VMs.
